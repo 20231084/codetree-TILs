1f52f0b82993e1cf89bd4a925859521c8b72dd8e
20231084 <150573988+20231084@users.noreply.github.com>
2023-12-10 00:14:38 +0900

    Title: 세 정수의 최솟값, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231210/세 정수의 최솟값/minimum-of-three-numbers.c
+++ 231210/세 정수의 최솟값/minimum-of-three-numbers.c
@@ -3,11 +3,15 @@
 int main() {
     int a, b, c;
     scanf("%d %d %d", &a, &b, &c);
-    if(a>=b && b>=c)
-        printf("%d", c);
-    else if(b>=c && c>=a)
+    if(a<=b && a<=c)
+    {
         printf("%d", a);
-    else    
+    }
+    else if(b<=a && b<=c)
+    {
         printf("%d", b);
+    }
+    else    
+        printf("%d", c);
     return 0;
 }
