cb20d8dcdce5f5152ae85069ec3b19c1f1b4fe91
20231084 <150573988+20231084@users.noreply.github.com>
2023-12-10 15:22:21 +0900

    Title: 두 사람, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231210/두 사람/two-person.c
+++ 231210/두 사람/two-person.c
@@ -5,7 +5,7 @@ int main() {
     scanf("%d %c", &age1, &gen1);
     scanf("%d %c", &age2, &gen2);
 
-    if(age1>=19 || age2>=19 && gen1=='M' || gen2=='M')
+    if((age1>=19 && age2>=19) || (gen1=='M' && gen2=='M'))
         printf("1");
     else
         printf("0");
