2078472af3d010a62a72b75771382b4f5d946493
20231084 <150573988+20231084@users.noreply.github.com>
2023-12-10 23:58:08 +0900

    Title: 배수의 개수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231210/배수의 개수 2/number-of-multipliers-2.c
+++ 231210/배수의 개수 2/number-of-multipliers-2.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    int i;
+    int i, a;
     int cnt=0;
     for(i=1; i<=10; i++)
     {
-        if(i%2==1)
-        {
+        scanf("%d", &a);
+        if(a%2==1)
             cnt++;
-        }
     }
     printf("%d\n", cnt);
 
