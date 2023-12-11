6a0936cf12e13bac8bb2dea9b2848eea4c52ff72
20231084 <150573988+20231084@users.noreply.github.com>
2023-12-12 02:58:33 +0900

    Title: 완전수 판별기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231212/완전수 판별기/perfect-number-discriminator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int sum=0;
+    scanf("%d", &n);
+    for(i=1; i<n; i++)
+    {
+        if(n%i==0)
+            sum+=i;
+    }
+    if(sum==n)
+        printf("P");
+    else
+        printf("N");
+    return 0;
+}
