5ef6d472a127c482b779e5a02caf93c0fb3eaacf
20231084 <150573988+20231084@users.noreply.github.com>
2023-12-12 20:51:42 +0900

    Title: 1부터 곱셈하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231212/1부터 곱셈하기/multiple-from-one.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int sum=0;
+    scanf("%d", &n);
+    for(i=1; i<=10; i++)
+    {
+        sum+=i;
+        if(sum>=n)
+        {
+            printf("%d", i);
+            break;
+        }
+    }
+    return 0;
+}
