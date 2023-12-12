ae7de6a1c4075fefafad6bcd9c7dc25703eb2528
20231084 <150573988+20231084@users.noreply.github.com>
2023-12-12 20:02:39 +0900

    Title: 친근하지 않은 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231212/친근하지 않은 수/unfriendly-number.c
+++ 231212/친근하지 않은 수/unfriendly-number.c
@@ -6,7 +6,7 @@ int main() {
     scanf("%d", &n);
     for(i=1; i<=n; i++)
     {
-        if(i%2==0 || i%3==0 || i%6==0)
+        if(i%2==0 || i%3==0 || i%5==0)
             continue;
         cnt++;
     }
