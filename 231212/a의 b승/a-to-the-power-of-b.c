98e89e6f65325ef01bcef8336d4decedb53666ea
20231084 <150573988+20231084@users.noreply.github.com>
2023-12-12 19:42:31 +0900

    Title: a의 b승, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231212/a의 b승/a-to-the-power-of-b.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, i;
+    int prod=1;
+    scanf("%d %d", &a, &b);
+    for(i=1; i<=b; i++)
+    {
+        prod*=a;
+    }
+    printf("%d", prod);
+    return 0;
+}
