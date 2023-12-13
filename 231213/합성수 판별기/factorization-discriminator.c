42a040410134b606d5d693774f453b7a7024c01d
20231084 <150573988+20231084@users.noreply.github.com>
2023-12-13 15:38:57 +0900

    Title: 합성수 판별기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231213/합성수 판별기/factorization-discriminator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<stdbool.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    bool satisfied=false;
+    for(i=2; i<n; i++)
+    {
+        if(n%i==0)
+            satisfied=true;
+    }
+    if(satisfied==true)
+        printf("C");
+    else
+        printf("N");
+    return 0;
+}
